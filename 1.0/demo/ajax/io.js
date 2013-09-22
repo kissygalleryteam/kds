0bec7e1e805ea219db56c132f26a2d79be01616f
zhangting <zhangting@taobao.com>
2013-09-22 15:46:43 +0800

    ajax先放放了。。

--- 1.0/demo/ajax/io.js
+++ 1.0/demo/ajax/io.js
@@ -2,7 +2,25 @@
     "package": "ajax",
     "name": "io",
     "doc": "http://docs.kissyui.com/docs/html/api/core/ajax/index.html",
-    "desc": "",
-    "tip": "",
-    "demo": []
+    "desc": "ajax是重要的kissy模块之一，通过它才能进行异步交互",
+    "demo": [
+    	{
+    		"title":"简单的get请求",
+    		"code":"http://jsfiddle.net/czy88840616/fC5za/embedded/"
+    	},
+    	{
+    		"title":"简单的post请求",
+    		"tip":"一般ajax都使用post",
+    		"code":"http://jsfiddle.net/czy88840616/56hKU/embedded/"
+    	},
+    	{
+    		"title":"获取纯json的话可以用getJSON",
+    		"code":"http://jsfiddle.net/czy88840616/McXs4/embedded/"
+    	},
+    	{
+    		"title":"jsonp请求",
+    		"tip":"可以直接使用KISSY.jsonp，别名而已",
+    		"code":"http://jsfiddle.net/czy88840616/QDxYX/embedded/"
+    	}
+    ]
 }
--- 1.0/demo/dom/selector.js
+++ 1.0/demo/dom/selector.js
@@ -2,7 +2,12 @@
     "package": "dom",
     "name": "selector",
     "doc": "http://docs.kissyui.com/docs/html/api/core/dom/selector.html",
-    "desc": "",
-    "tip": "",
-    "demo": []
+    "desc": "KISSY本身的选择器对比jquery起来不算丰富，但是如果html写的好的话，完全可以覆盖大部分场景",
+    "tip": "如果希望支持其他选择器，请加载sizzle模块",
+    "demo": [
+    	{
+    		"title":"官方demo",
+    		"code":"http://docs.kissyui.com/docs/html/api/core/dom/selector.html"
+    	}
+    ]
 }
