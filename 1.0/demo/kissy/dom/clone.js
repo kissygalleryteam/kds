ff2592e4a153e1798df993a83ab3985ec64bc7d8
zhangting <zhangting@taobao.com>
2013-10-10 09:55:38 +0800

    提了几个dom

--- 1.0/demo/kissy/dom/clone.js
+++ 1.0/demo/kissy/dom/clone.js
@@ -2,8 +2,14 @@
     "package": "dom",
     "name": "clone",
     "doc": "http://docs.kissyui.com/docs/html/api/core/dom/clone.html",
-    "desc": "",
-    "tip": "",
+    "desc": "返回一个dom元素的克隆",
+    "tip": "普通对象和普通数组请用 KISSY.clone",
     "version":">=1.2",
-    "demo": []
+    "demo": [
+    	{
+    		"title":"简单的demo",
+    		"code":"http://demo.kissyui.com/#!/detail/gUGzHM",
+    		"author":"堂主"
+    	}
+    ]
 }
